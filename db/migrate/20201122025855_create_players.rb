e1e78a4e5533e1c59fc1f04ddd0de0c610898e6c
raraibo <raraibo@yahoo.co.jp>
2020-11-22 13:23:24 +0900

    マイグレーションファイルのnot null記法が誤っていたのを修正

--- db/migrate/20201122025855_create_players.rb
+++ db/migrate/20201122025855_create_players.rb
@@ -1,7 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration[6.0]
   def change
     create_table :players do |t|
-      t.string :name, :null false
+      t.string :name, null: false
       t.string :twitter_id
       t.string :youtube_url
       t.string :note
