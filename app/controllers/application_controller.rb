a9eaf6fb857a0ed49431f517df275082f399fc39
raraibo <raraibo@yahoo.co.jp>
2020-11-23 14:02:48 +0900

    スプレッドシートの接続設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+
+  def google_drive
+    credentials = Google::Auth::UserRefreshCredentials.new(
+      client_id: ENV['GOOGLE_CLIENT_ID'],
+      client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+      scope: %w(https://www.googleapis.com/auth/drive https://spreadsheets.google.com/feeds/),
+      redirect_uri: 'http://example.com/redirect'
+    )
+    credentials.refresh_token = ENV['GOOGLE_REFRESH_TOKEN']
+    credentials.fetch_access_token!
+    session = GoogleDrive::Session.from_credentials(credentials)
+
+    ws = session.spreadsheet_by_key("xxxxxxxxxxxxxxxxxxxxxxxxxxx").worksheets[0]
+
+    p ws[0, 0]
+  end
 end
